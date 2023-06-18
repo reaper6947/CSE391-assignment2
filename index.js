fc1926c30f61db5ed3fc7d5fe6db04b5782315e7
mahid atif hosain <50234055+reaper6947@users.noreply.github.com>
2023-06-18 05:10:11 +0000

    updated

--- index.js
+++ index.js
@@ -51,6 +51,84 @@ document.querySelector('.list-input').addEventListener('input', function () {
 
     reverseVal.innerText = [...inputText.split(',')].reverse().join(',')
 
+})
+
+document.getElementById('clear-all').addEventListener('click', function () {
+    document.getElementById('text-area').value = ""
 
 })
+
+document.getElementById('capitalize').addEventListener('click', function () {
+    const mySentence = document.getElementById('text-area').value
+    const words = mySentence.split(" ")
+    if (document.getElementById('text-area').dataset.capitalize == '1') {
+        for (let i = 0; i < words.length; i++) {
+            if (words[i] != "") words[i] = words[i][0].toLowerCase() + words[i].substr(1)
+        }
+        document.getElementById('text-area').value = words.join(" ")
+
+        const mySentence2 = document.getElementById('text-area').value
+        const words2 = mySentence2.split(/\r?\n/)
+        console.log(words2)
+        for (let i = 0; i < words2.length; i++) {
+            if (words2[i] != "") words2[i] = words2[i][0].toLowerCase() + words2[i].substr(1)
+        }
+        document.getElementById('text-area').value = words2.join("\n")
+        document.getElementById('text-area').dataset.capitalize = '0'
+    } else {
+        for (let i = 0; i < words.length; i++) {
+            if (words[i] != "") words[i] = words[i][0].toUpperCase() + words[i].substr(1)
+        }
+        document.getElementById('text-area').value = words.join(" ")
+        const mySentence2 = document.getElementById('text-area').value
+        const words2 = mySentence2.split(/\r?\n/)
+        console.log(words2)
+        for (let i = 0; i < words2.length; i++) {
+            if (words2[i] != "") words2[i] = words2[i][0].toUpperCase() + words2[i].substr(1)
+        }
+        document.getElementById('text-area').value = words2.join("\n")
+        document.getElementById('text-area').dataset.capitalize = '1'
+    }
+})
+
+
+document.getElementById('sort').addEventListener('click', function () {
+    document.getElementById('text-area').value = document.getElementById('text-area').value.trim()
+
+    const sortLines = str => document.getElementById('text-area').value.split(/\r?\n/).sort().join('\n');
+    document.getElementById('text-area').value = sortLines(document.getElementById('text-area').value)
+})
+
+document.getElementById('reverse').addEventListener('click', function () {
+    document.getElementById('text-area').value = document.getElementById('text-area').value.trim()
+    const sortLines = str => document.getElementById('text-area').value.split(/\r?\n/).reverse().join('\n');
+    document.getElementById('text-area').value = sortLines(document.getElementById('text-area').value)
+})
+
+document.getElementById('strip-blank').addEventListener('click', function () {
+    document.getElementById('text-area').value = document.getElementById('text-area').value.trim()
+    document.getElementById('text-area').value = document.getElementById('text-area').value.replace(/^(?=\n)$|^\s*|\s*$|\n\n+/gm, "")
+})
+
+document.getElementById('add-numbers').addEventListener('click', function () {
+    const mySentence2 = document.getElementById('text-area').value
+    const words2 = mySentence2.split(/\r?\n/)
+    const newWords = words2.map(el => '6' + el)
+    document.getElementById('text-area').value = newWords.join('\n')
+})
+
+
+document.getElementById('shuffle').addEventListener('click', function () {
+    const mySentence2 = document.getElementById('text-area').value
+    const words2 = mySentence2.split(/\r?\n/)
+    const newar = words2.sort(() => Math.random() - 0.5);
+    document.getElementById('text-area').value = newar.join('\n')
+    
+})
+
+
+
+
+
+
 
